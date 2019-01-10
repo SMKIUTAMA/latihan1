cdd3644956191c2e5ee0b62192ba5add53f75966
WahyuAdiPramudya <46449245+WahyuAdiPramudya@users.noreply.github.com>
2019-01-10 11:34:59 +0700

    tambahan file update.php

---
+++ proses_update.php
@@ -0,0 +1,32 @@
+<?php 
+    include "koneksi.php";
+
+    $ID = $_POST["id"];
+    $NAMA = $_POST["nama"];
+    $JENKEL = $_POST["jenkel"];
+    $KELAS = $_POST["kelas"];
+    $ALAMAT = $_POST["alamat"];
+    $No_hape = $_POST["telp"];
+
+    $query = "UPDATE tb_latihan1 INTO (id,nama,jenis_kelamin,kelas,alamat,no_telp)
+            SET
+         id = '$ID', nama = '$NAMA', 
+         jenis_kelamin = '$JENKEL', 
+         alamat = '$ALAMAT', 
+         no_telp='$No_hape' 
+         WHERE id = $_GET[id]";
+
+    $sql = mysqli_query($conn,$query);
+
+         if($update == true){
+			echo"<script>
+			    	alert('Data Berhasil di update');
+					document.location.href='index.php';
+				</script>";
+		}else{
+			echo"<script>
+					alert('Data Gagal di update');
+					document.location.href='index.php';
+                </script>";
+    }
+?>
