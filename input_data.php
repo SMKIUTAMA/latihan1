1f46996051480f5d36aba75c40c46cd3260ed95e
Septianasr23 <46474943+Septianasr23@users.noreply.github.com>
2019-01-10 11:36:41 +0700

    udah dirapihin tuh

--- input_data.php
+++ input_data.php
@@ -1,15 +1,15 @@
 <?php
 	include "koneksi.php";
 
-$Nama		    = $_POST['nama'];
+$Nama		= $_POST['nama'];
 $Jenis_kelamin  = $_POST['jenis_kelamin'];
-$Kelas  		= $_POST['kelas'];
-$Alamat			= $_POST['alamat'];
+$Kelas          = $_POST['kelas'];
+$Alamat         = $_POST['alamat'];
 $No_telp        = $_POST['no_telp'];
 
 
 $query = "INSERT into tb_siswa (nama,jenis_kelamin,kelas,alamat,no_telp) 
-		  VALUES ('$Nama','$Jenis_kelamin','$Kelas','$Alamat','$No_telp')";
+	  VALUES ('$Nama','$Jenis_kelamin','$Kelas','$Alamat','$No_telp')";
 $hasil = mysqli_query($koneksi,$query);
 	
 if ($hasil){
