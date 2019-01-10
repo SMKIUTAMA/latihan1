dc1b5d8b783df135b92cbb949d37fcd17a74c389
danangts <danangtskhair07islm@gmail.com>
2019-01-10 10:50:31 +0700

    penambahan

--- form_update.php
+++ form_update.php
@@ -2,8 +2,65 @@
 <html>
 <head>
 	<title>View|form edit</title>
-</head>
-<body>
+	<style>
+				body {
+						position: relative;
+					   	top:100px;
+					 }
+				h1 {
+						position: relative;
 
+					}
+		</style>
+</head>
+ <body>
+ 	<?php 
+ 		
+ 	 ?>
+	<form action="proses_update.php" method="post">
+<center>
+<h1> Form edit data </h1>
+<br>
+	<table>
+<tr>
+	<td>ID</td>
+	<td><input type="text" name="id" value="" readonly></td>
+</tr>
+<tr> 
+	<td> Nama </td>
+	<td> <input type="text" name="nama" value=""></td>
+</tr>
+<tr> 
+	<td> Jenis Kelamin </td>
+	<td> 
+		<input type="radio" name="jenkel" value="laki"> Laki-Laki
+		 <input type="radio" name="jenkel" value="perempuan"> Perempuan
+    </td>
+</tr>
+<tr> 
+	<td> Kelas </td>
+	<td>
+		 <input type="text" name="kelas" value="">
+	</td>
+</tr>
+<tr> 
+	<td> Alamat </td>
+	<td> 
+		 <textarea name="alamat" ></textarea>
+	</td>
+</tr>
+<tr> 
+	<td> No Telepon </td>
+	<td>
+		 <input type="text" name="telp" value="">
+	</td>
+</tr>
+<tr> 
+	<td>
+		<input type="submit" name="edit" value="Edit">
+	</td>
+	<td>
+		<input type="reset" name="bersih" value="Batal">
+	</td>
 </body>
 </html>
