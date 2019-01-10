c40b5fdae07dde20055e29819275c743936fc410
Septianasr23 <septiasari231@gmail.com>
2019-01-10 12:21:12 +0700

    bilang-bilang dong

--- input_data.php
+++ input_data.php
@@ -1,19 +1,19 @@
 <?php
 	include "koneksi.php";
 
-$Nama		= $_POST['nama'];
+$Nama		    = $_POST['nama'];
 $Jenis_kelamin  = $_POST['jenkel'];
 $Kelas          = $_POST['kelas'];
 $Alamat         = $_POST['alamat'];
 $No_telp        = $_POST['telp'];
 
 
-$query = "INSERT into tb_siswa (nama,jenis_kelamin,kelas,alamat,no_telp) 
-	  VALUES ('$Nama','$Jenis_kelamin','$Kelas','$Alamat','$No_telp')";
+$query = "INSERT into tb_latihan1 (nama,jenis_kelamin,kelas,alamat,no_telp) 
+	      VALUES ('$Nama','$Jenis_kelamin','$Kelas','$Alamat','$No_telp')";
 $hasil = mysqli_query($conn ,$query);
 	
 if ($hasil){
-	header ("location:index.php");
+	header ("location:indexx.php");
 }
 else {
 	echo "Input Gagal";
