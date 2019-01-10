0799d39a547fec53c90df475e98a4173cdbb89f6
Zulkivlin <46513638+Zulkivlin@users.noreply.github.com>
2019-01-10 11:22:12 +0700

    ini tampilan indexnya

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include_once("config.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+
+<html>
+<head>    
+    <title>Tampilan</title>
+</head>
+
+<body>
+<a href="#">Tambah Data</a><br/><br/>
+
+    <table width='80%' border=1>
+
+    <tr>
+        <th>Id</th> <th>Nama</th> <th>Jenis Kelamin</th> <th>Kelas</th> <th>Alamat</th> <th>No Telepon</th> <th>Opsi</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['id']."</td>";
+        echo "<td>".$user_data['nama']."</td>";
+        echo "<td>".$user_data['jenis_kelamin']."</td>"; 
+        echo "<td>".$user_data['kelas']."</td>"; 
+        echo "<td>".$user_data['alamat']."</td>"; 
+        echo "<td>".$user_data['no_telp']."</td>"; 
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+</body>
+</html>
